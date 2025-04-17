42284f447d1609e65377f79b9bc19431681cab28
DemetrioQ <demetriorqe@gmail.com>
2025-04-16 23:47:19 -0400

    removed unused code

--- 2048-next/next.config.ts
+++ 2048-next/next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
-import type { Configuration as WebpackConfig } from 'webpack'
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  // webpack: (config: WebpackConfig, { dev, isServer }) => {
-  //   if (dev && !isServer) {
-  //     config.watchOptions = {
-  //       poll: 1000,
-  //       aggregateTimeout: 300,
-  //     }
-  //   }
-  //   return config
-  // },
 };
 
 export default nextConfig;
