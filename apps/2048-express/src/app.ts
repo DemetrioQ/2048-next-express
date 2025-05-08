8f0d30f680807c721443a1ee07c05fc91dccc126
DemetrioQ <demetriorqe@gmail.com>
2025-05-08 01:33:54 -0400

    Configured shared "package" for development, now can hot reload all 3 projects

--- apps/2048-express/src/app.ts
+++ apps/2048-express/src/app.ts
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/test', (req, res) => {
     const span = '<span style="text-decoration: underline;">app<span/>'
-    const heading = `<h1>This is ${span} hot reloade!<h1/>`
+    const heading = `<h1>This is ${span} hot reloaded!<h1/>`
     res.send(heading);
 })
 
