b10cc29998cddf8e5502b08060a5f57d866e422c
DemetrioQ <demetriorqe@gmail.com>
2025-10-17 23:19:39 -0400

    fixed  missing css

--- apps/2048-next/app/layout.tsx
+++ apps/2048-next/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientProviders from "./components/ClientProviders";
 import { Suspense } from 'react';
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
