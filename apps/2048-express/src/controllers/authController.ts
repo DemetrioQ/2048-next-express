d20bc57a351100cea40a8fe4a8fc92251b86e3c6
DemetrioQ <demetriorqe@gmail.com>
2025-10-18 14:10:04 -0400

    test

--- apps/2048-express/src/controllers/authController.ts
+++ apps/2048-express/src/controllers/authController.ts
@@ -158,21 +158,21 @@ export const handleOAuthCallback = async (req: Request, res: Response) => {
 
     res.cookie('refresh_token', refreshToken, baseCookieOptionsRefreshToken);
 
-    res.redirect(`${process.env.FRONTEND_URL}/oauth/success`);
-    //     res.send(`
-    //     <html>
-    //       <body>
-    //         <script>
-    //           if (window.opener) {
-    //             window.opener.postMessage({ type: 'oauth-success' }, '*');
-    //             window.close();
-    //           } else {
-    //             window.location.href = '${process.env.FRONTEND_URL}';
-    //           }
-    //         </script>
-    //       </body>
-    //     </html>
-    //   `);
+    // res.redirect(`${process.env.FRONTEND_URL}/oauth/success`);
+        res.send(`
+        <html>
+          <body>
+            <script>
+              if (window.opener) {
+                window.opener.postMessage({ type: 'oauth-success' }, '*');
+                window.close();
+              } else {
+                window.location.href = '${process.env.FRONTEND_URL}';
+              }
+            </script>
+          </body>
+        </html>
+      `);
 };
 
 export const logout = async (req: Request, res: Response) => {
